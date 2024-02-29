272c6344e437d81a01b73ecf3e4a7ee26534d34f
atricid <125633129+26cluu@users.noreply.github.com>
2024-02-29 09:15:32 -0800

    make draw method and cards and deck methods

--- Card.java
+++ Card.java
@@ -1,15 +1,31 @@
+import java.util.*;
 public class Card {
     
+    String suite, value;
+
     //to be changed to symbols later on
     String[] suites = {"diamonds", "cloves", "hearts", "spades"};
     //numbers and faces
     String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-    String[] currentCard = new String[2];
-
-
     public Card(String value, String suite) {
-        this.currentCard[0] = value;
-        this.currentCard[1] = suite;
+        this.value = value;
+        this.suite = suite;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public String getSuite() {
+        return this.suite;
+    }
+
+    public int getNumberValue(Card card) {
+        return Arrays.asList(values).indexOf(card.getValue()) + 1;
+    }
+
+    public int getSuiteValue(Card card) {
+        return Arrays.asList(suites).indexOf(card.getSuite()) + 1;
     }
 }
---
+++ Deck.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+public class Deck {
+    Random rand = new Random();
+
+    //to be changed to symbols later on
+    String[] suites = {"diamonds", "cloves", "hearts", "spades"};
+    //numbers and faces
+    String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    ArrayList<Card> deck = new ArrayList<Card>();
+
+    public Deck() {
+        for (String suite: suites) {
+            for (String value : values) {
+                Card current = new Card(value, suite);
+                deck.add(current);
+            }
+        }
+    }
+
+    public void draw(ArrayList<Card> hand) {
+        int deckNumber = rand.nextInt(52);
+        hand.add(deck.get(deckNumber));
+        deck.remove(deckNumber);
+    }
+
+}
